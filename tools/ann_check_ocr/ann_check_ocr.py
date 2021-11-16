8093ce716091780f1d020e6c76380a1b71a4d7be
Jungyeon Song <gldkzm@gmail.com>
2021-11-16 15:32:26 +0000

    chore: set git remote at opt/ml/

---
+++ tools/ann_check_ocr/ann_check_ocr.py
@@ -0,0 +1,93 @@
+#annotation_check.py
+
+import cv2
+import os
+import json
+import numpy as np
+import my_path
+
+
+img_path = my_path.img_path                        #
+json_path = my_path.json_path                      # my_path.py 파일에서 자신의 경로로 바꿔주세요!
+
+# annotation data 읽기
+with open(json_path, 'r', encoding='UTF-8') as f:
+    json_data = json.load(f)
+img_list = list(json_data['images'].keys())
+# 이상한 이미지 목록 있다면 불러오기
+Strange_img_set = set()
+if os.path.exists('./StrangeImgList.txt'):
+    with open('StrangeImgList.txt', 'r') as f:
+        read_data = f.readline()
+        if len(read_data) >= 1: # 저장된 요소가 있다면
+            read_data = read_data.split(',')
+            print('exist_datas', read_data) # 기존 데이터 출력
+            Strange_img_set = set(read_data)\
+
+
+####### 필요에 따른 수정 부분
+current_idx = 0 # 시작 인덱스
+end_idx = 275 # 끝 인덱스
+
+img_size = (800, 800) # 편의에 맞게 이미지 크기 조절
+img_save = True # 이미지 저장 여부
+
+if img_save:
+    if not os.path.exists("./StrangeImage"):
+        os.mkdir("./StrangeImage")
+
+# txt 파일 업데이트
+def save_txt(img_set):
+    write_elements = ''
+    if len(img_set) >= 1: # 저장할 요소가 있다면
+        Strange_img_list = sorted(list(img_set)) # 정렬후, 저장
+        write_elements = list(map(str, Strange_img_list))
+        write_elements = ",".join(write_elements)
+        print('save_datas', write_elements)
+    with open('StrangeImgList.txt', 'w') as f:
+        f.write(write_elements)
+
+
+while True:
+    current_img = img_list[current_idx]
+    img = cv2.imread(os.path.join(img_path, current_img))
+    words = json_data['images'][current_img]['words']
+
+    # 글자 도형 그리기
+    for word in words.values():
+        if word['illegibility']:
+            color = (0,0,255)
+        else:
+            color = (100,255,0)
+        points = np.array(word['points'], dtype=np.int32)
+        points = points.reshape((-1,1,2))
+        img = cv2.polylines(img, [points], True, color, 2)
+
+    img = cv2.resize(img, img_size, interpolation = cv2.INTER_AREA)
+    cv2.imshow("win", img)
+    ret = cv2.waitKey(0)
+    if (ret == 102 or ret == 70) and current_idx != end_idx: # 'F' or 'f' 입력한 경우, Foward
+        current_idx += 1
+        print('current_idx', current_idx)
+    elif (ret == 98 or ret == 66) and current_idx != 0: # 'B' or 'b' 입력한 경우, Backward
+        current_idx -= 1
+        print('current_idx', current_idx)
+    elif ret == 83 or ret == 115: # 'S' or 's' 입력한 경우, Save
+        if img_save: # 이미지 저장
+            if not os.path.exists("./StrangeImage/{}".format(current_img)):
+                cv2.imwrite("./StrangeImage/{}".format(current_img), img)
+        Strange_img_set.add(current_img)
+        save_txt(Strange_img_set) # 텍스트 파일 업데이트
+        print('add', current_img)
+    elif ret == 68 or ret == 100: # 'D' or 'd' 입력한 경우, Delete
+        try:
+            Strange_img_set.remove(current_img)
+            print('del', current_img)
+            save_txt(Strange_img_set) # 텍스트 파일 업데이트
+        except:
+            pass
+        if img_save: # 이미지 제거
+            if os.path.exists("./StrangeImage/{}".format(current_img)):
+                os.remove("./StrangeImage/{}".format(current_img))
+    elif ret == 27: # 'ESC' 입력시 종료
+        break
---
+++ tools/make_json/make_json.py
@@ -0,0 +1,37 @@
+import json
+import os
+
+json_path = 'annotation.json'
+json_path2 = 'train.json'
+
+with open(json_path, 'r', encoding='UTF-8') as f:
+    json_data = json.load(f)
+
+with open(json_path2, 'r', encoding='UTF-8') as f:
+    json_data2 = json.load(f)
+
+if os.path.exists('StrangeImgList.txt'):
+    with open('StrangeImgList.txt', 'r', encoding='UTF-8') as f:
+        strange_data = f.readline()
+        if len(strange_data) >= 1: # 저장된 요소가 있다면
+            read_data = strange_data.split(',')
+
+print(len(json_data['images']))
+for fname in read_data:
+    try:
+        json_data['images'].pop(fname)
+    except:
+        pass
+print(len(json_data['images']))
+for fname in json_data['images']:
+    words = json_data['images'][fname]['words']
+    for k, v in tuple(words.items()):
+        if len(v['points'])!=4:
+            print(v['points'])
+            json_data['images'][fname]['words'].pop(k)
+print(len(json_data['images']))
+json_data['images'].update(json_data2['images'])
+print(len(json_data['images']))
+
+# with open('new_ann2.json', 'w', encoding='UTF-8') as f:
+#     json.dump(json_data,f,indent=2)
